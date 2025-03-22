733dad4c4d0f5ea56b8edd75514ff1c3bb924277
urvashi <urvashikshatriya9@gmail.com>
2025-03-22 23:55:15 +0530

    all the pages of the project has been created

--- mess/views.py
+++ mess/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+def messbill(request):
+    return render(request, "messbill.html")
+
+def personalInfo(request):
+    return render(request, "personalInfo.html")
+
+def staffInfo(request):
+    return render(request, "staffInfo.html")
+
+def studentInfo(request):
+    return render(request, "studentProf.html")
+
+def complain(request):  
+    return render(request, "complain.html")
