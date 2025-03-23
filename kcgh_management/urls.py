9b88edd4aa202127ae4cfd35f3d224e8892b673c
Shimaila <hshimaila@gmail.com>
2025-03-23 15:48:52 +0530

    Home Page has been created

--- kcgh_management/urls.py
+++ kcgh_management/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mess.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
